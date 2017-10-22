4dba9bc76db171950cbf735d7bc54a6abefc3619
dderaad <dderaad@uw.edu>
2017-10-22 02:37:45 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,20 +1,43 @@
 # import libraries
 # import urllib2
 import urllib
+import urllib.request as ur
 from bs4 import BeautifulSoup
+import lxml.html
 
-def scrape_asl_url(page_no, url):
-    assert(len(page_no) >= 4)
+glosses = ['ALSO']
+#quote_page = 'http://secrets.rutgers.edu/dai/queryPages/search/result.php?type=partial&key=also&variant_name=' + gloss + '&demonstrator=All&is_main=1'
 
-    # query the website and return the html to the variable
-    quote_page = 'https://www.handspeak.com/word/search/index.php?id='
-    quote_page += str(page_no)
-    quote_page = url
-    print(quote_page)
-    page = urllib.urlopen(quote_page)
-    soup = BeautifulSoup(page, 'html.parser') # parsing the html using beautiful soup and store in variable
+def scrape_asl_url(gloss_list):
+    gtu = {}
+    for gloss in gloss_list:
+        quote_page = 'http://secrets.rutgers.edu/dai/queryPages/search/result.php?type=partial&key=also&variant_name={}&demonstrator=All&is_main=1'.format(gloss)
+        print(gloss)
+        page = ur.urlopen(quote_page)
+        soup = BeautifulSoup(page, 'html.parser') # parsing the html using beautiful soup and store in variable
 
-    print(soup)
+        #print(soup)
+        """
+        for link in soup.find_all('a'):
+            links = (link.get('href'))
+            print(links)
+            return links
+        """
+        gtu[gloss] = set()
+        tags = soup.find_all('a')
+        for tag in tags:
+            url = tag.get('href')
+            url = url[url.find('url=')+4:]
+            gtu[gloss].add(url)
 
 
-scrape_asl_url("0001", 'http://secrets.rutgers.edu/dai/queryPages/search/result.php?type=partial&key=also&variant_name=ALSO&demonstrator=All&is_main=1')
+    return gtu
+
+
+with open("glosses") as f:
+    glosses = []
+    for gloss in f:
+        glosses.append(gloss.rstrip())
+
+html_dict = scrape_asl_url(glosses)
+print(html_dict)
